8693ad8e94731b08e639422e153235ad5b52a395
pantifik <a.markelychev@gmail.com>
2018-01-22 02:04:46 +0300

    fix iss#1

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,17 @@
 const gulp        = require('gulp'),
-      browsersync = require('browser-sync').create(),
-      less        = require('gulp-less'),
-      replace     = require('gulp-replace'),
-      cheerio     = require('gulp-cheerio'),
-      svgSprite   = require('gulp-svg-sprite'),
-      svgmin      = require('gulp-svgmin'),
-      sourcemap   = require('gulp-sourcemaps'),
-      autopref    = require('gulp-autoprefixer'),
-      csscomb     = require('gulp-csscomb'),
-      gcmq        = require('gulp-group-css-media-queries'),
-      pug         = require('gulp-pug'),
-      notify      = require('gulp-notify'),
-      clean       = require('del');
+  browsersync = require('browser-sync').create(),
+  less        = require('gulp-less'),
+  replace     = require('gulp-replace'),
+  cheerio     = require('gulp-cheerio'),
+  svgSprite   = require('gulp-svg-sprite'),
+  svgmin      = require('gulp-svgmin'),
+  sourcemap   = require('gulp-sourcemaps'),
+  autopref    = require('gulp-autoprefixer'),
+  csscomb     = require('gulp-csscomb'),
+  gcmq        = require('gulp-group-css-media-queries'),
+  pug         = require('gulp-pug'),
+  notify      = require('gulp-notify'),
+  clean       = require('del');
 
 const config = {
   product: {
@@ -60,41 +60,41 @@ gulp.task('browsersync', function () {
 
 gulp.task('less', function () {
   return gulp.src([config.less.src])
-      .pipe(sourcemap.init())
-      .pipe(less())
-      .on('error', notify.onError(function(error) {
-       return {
-         title: 'LESS',
-         message: error.message
-       };
-      }))
-      .pipe(gcmq())
-      .pipe(autopref({
-        browsers: ['last 2 versions']
-      }))
-      .pipe(csscomb())
-      .pipe(sourcemap.write())
-      .pipe(gulp.dest(config.less.dest))
-      .pipe(browsersync.reload({
-        stream: true
-      }));
+             .pipe(sourcemap.init())
+             .pipe(less())
+             .on('error', notify.onError(function(error) {
+               return {
+                 title: 'LESS',
+                 message: error.message
+               };
+             }))
+             .pipe(gcmq())
+             .pipe(autopref({
+               browsers: ['last 2 versions']
+             }))
+             .pipe(csscomb())
+             .pipe(sourcemap.write())
+             .pipe(gulp.dest(config.less.dest))
+             .pipe(browsersync.reload({
+               stream: true
+             }));
 });
 
 gulp.task('pug', function () {
   return gulp.src([config.pug.src])
-      .pipe(pug({
-        pretty: true
-      }))
-      .on('error', notify.onError(function(error) {
-       return {
-         title: 'Pug',
-         message: error.message
-       };
-      }))
-      .pipe(gulp.dest(config.pug.dest))
-      .pipe(browsersync.reload({
-        stream: true
-      }));
+             .pipe(pug({
+               pretty: true
+             }))
+             .on('error', notify.onError(function(error) {
+               return {
+                 title: 'Pug',
+                 message: error.message
+               };
+             }))
+             .pipe(gulp.dest(config.pug.dest))
+             .pipe(browsersync.reload({
+               stream: true
+             }));
 });
 
 gulp.task('svg', function () {
@@ -134,20 +134,20 @@ gulp.task('svg', function () {
 
 gulp.task('fonts', function() {
   return gulp.src(config.fonts.src)
-        .pipe(gulp.dest(config.fonts.dest));
+             .pipe(gulp.dest(config.fonts.dest));
 });
 
 gulp.task('img', function() {
   return gulp.src(config.img.src)
-        .pipe(gulp.dest(config.img.dest));
+             .pipe(gulp.dest(config.img.dest));
 });
 
 gulp.task('js', function() {
   return gulp.src(config.js.src)
-        .pipe(gulp.dest(config.js.dest))
-        .pipe(browsersync.reload({
-          stream: true
-        }));
+             .pipe(gulp.dest(config.js.dest))
+             .pipe(browsersync.reload({
+               stream: true
+             }));
 });
 
 gulp.task('clean', function() {
@@ -164,6 +164,6 @@ gulp.task('watch', function () {
   gulp.watch(config.product.dest + '/**/*.html', browsersync.reload);
 });
 
-gulp.task('dev', gulp.series('clean', gulp.parallel('pug', 'less', 'js', 'svg', 'fonts')));
+gulp.task('dev', gulp.series('clean', gulp.parallel('pug', 'less', 'js', 'svg', 'fonts', 'img')));
 
 gulp.task('default', gulp.series('dev', gulp.parallel('browsersync', 'watch') ));
